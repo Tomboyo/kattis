35219a144468307c1e2618f32456aee258c46054
Tom Simmons <tomasimmons@gmail.com>
2018-06-03 04:21:34 -0400

    Add solution description

--- solutions/src/main/java/com/github/tomboyo/bobbysbet/Solution.java
+++ solutions/src/main/java/com/github/tomboyo/bobbysbet/Solution.java
@@ -1,10 +1,5 @@
-package com.github.tomboyo.bobbysbet;
-
-import com.kattis.open.Kattio;
-import java.io.*;
-import java.util.function.Function;
-
 /*
+PROBLEM DESCRIPTION:
 Bobby and Betty have a bet. Betty bets Bobby that he cannot roll an S-sided die
 (having values 1 through S) and obtain a value ≥R on at least X out of Y rolls.
 Betty has a variety of dice with different numbers of sides S, and all her dice
@@ -20,7 +15,34 @@ Input
 Input begins with an integer 1≤N≤10000, representing the number of cases that
 follow. The next N lines each contain five integers, R, S, X, Y, and W. Their
 limits are 1≤R≤S≤20, 1≤X≤Y≤10, and 1≤W≤100.
+
+SOLUTION DESCRIPTION:
+This solution uses a combinatorial approach. Based on the S, R, X, and Y
+parameters, we determine the proportion of expected successful attempts, where 1
+indicates that all attempts should succeed and 0 indicates that all attempts
+should fail. We multiply this proportion by the payoff W; if this value is
+greater than 1, we expect that Bobby will profit over time and should therefore
+take the bet.
+
+Each time Bobby makes an attempt, there are a variety of ways in which he can
+roll at least X successes: Bobby could get exactly X successes, exactly X + 1
+successes, exactly X + 2, and so on up to Y. Each way with exactly k successes 
+has a probability of occurring. We determine this probability by calculating
+the probability of a successful die result (R or higher) and the probability
+of an unsuccessful die result (less than R), and taking an appropriate product.
+That is, we take the success probability to the power of k and the failure
+probability to the power of Y – k, then multiply them together, thus giving us
+the likelihood that we roll exactly k successes and Y – k failures on Y dice.
+Finally, we sum the likelihoods for each possible value of k (which ranges from
+X to Y), giving us the proportion of attempts which result in success.
 */
+
+package com.github.tomboyo.bobbysbet;
+
+import com.kattis.open.Kattio;
+import java.io.*;
+import java.util.function.Function;
+
 public class Solution {
   private Kattio kattio;
 
