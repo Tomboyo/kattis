d3e5ebce84a767f302dadd69fa82319891b1fea6
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2018-07-27 22:39:55 -0400

    Reimplement interval tree
    Backing list of intervals is spliced on insert. Will add a BST top index
    list members next.

--- ruby-solutions/lib/interval_tree.rb
+++ ruby-solutions/lib/interval_tree.rb
@@ -1,95 +1,162 @@
 require 'set'
 
-# Associates values with integer ranges.
-class IntervalTree
-  attr_reader :left, :right, :range, :values
+class Interval
+  attr_accessor :low, :high
 
-  def initialize(range)
-    @range = range
-    @values = []
+  def initialize low, high
+    if !high.kind_of?(Numeric) || !low.kind_of?(Numeric)
+      throw "#{low}, #{high} must be numbers"
+    end
+
+    @low = low
+    @high = high
   end
 
-  def insert(range, value)
-    if IntRanges.a_covers_b? @range, range
-      @values << value
-    else
-      if (@left.nil? || @right.nil?) && @range.size > 1
-        low = @range.first
-        high = @range.end
-        middle = (@range.size / 2) + low - 1
-        @left ||= IntervalTree.new(low..middle)
-        @right ||= IntervalTree.new((middle + 1)..high)
-      end
-
-      if IntRanges.intersect? @left.range, range
-        @left.insert range, value
-      end
-
-      if IntRanges.intersect? @right.range, range
-        @right.insert range, value
-      end
-    end
+  # Shares no common points with interval other
+  def disjoint? other
+    @high < other.low ||
+        @low > other.high
   end
 
-  def query(range, result_set = Set.new)
-    if IntRanges.intersect? @range, range
-      result_set.merge @values
-    end
-      
-    if IntRanges.intersect? @left&.range, range
-      @left.query range, result_set
-    end
+  # Endpoints are contained by interval other
+  def subinterval_of? other
+    other.low <= @low &&
+        @high <= other.high
+  end
 
-    if IntRanges.intersect? @right&.range, range
-      @right.query range, result_set
-    end
-
-    result_set
+  # Is a subinterval of other and does not share endpoints
+  # with other
+  def proper_subinterval_of? other
+    other.low < @low &&
+        @high < other.high
   end
 
   def to_s
-    "[#{@range}]=>#{@values.to_s}"
-  end
-
-  def pretty_print(indent = 0, string = "")
-    string << " " * indent << to_s << "\n"
-    @left&.pretty_print(indent + 2, string)
-    @right&.pretty_print(indent + 2, string)
-    string
+    "[#{@low},  #{@high}]"
   end
 end
 
-module IntRanges
-  # Test if the first range covers the second range
-  # a_covers_b?(1..10, 2..9) == true
-  # a_covers_b?(1..10, 1..10) == true
-  # a_covers_b?(1..10, 1..11) == false
-  # a_covers_b?(1..10, 0..10) == false
-  def self.a_covers_b?(a, b)
-    b.first <= a.first &&
-        a.end <= b.end
+class  IntervalList
+  attr_accessor :interval, :values, :next
+
+  def initialize(interval, values = [], next_node = nil)
+    if !values.kind_of?(Array)
+      throw "Values not kind of array!"
+    end
+
+    @interval = interval
+    @values = values
+    @next = next_node
   end
 
-  # Test if two ranges intersect
-  # intersect?(1..2, 2..4) == true
-  # intersect?(1...2, 2..4) == false
-  def self.intersect?(a, b)
-    if a.nil? || b.nil?
-      false
+  def low
+    @interval.low
+  end
+
+  def high
+    @interval.high
+  end
+
+  # Subdivides the current list-node in-place about the
+  # given interval and returns an array containing the
+  # sublist nodes thus created or modified.
+  #
+  # If a list node containing [1..5] is split about [2..2],
+  # the result will be an array of three nodes: [1..1],
+  # [2..2], [3..5].
+  def split_around! interval
+    if @interval.subinterval_of?(interval) ||
+        @interval.disjoint?(interval)
+      [self]
+    elsif interval.proper_subinterval_of? @interval
+      trisect_around! interval
+      [self, @next, @next.next]
     else
-      last_in(a) >= b.first &&
-          a.first <= last_in(b)
+      if low < interval.low
+        bisect_after! interval.low - 1
+      else
+        bisect_after! interval.high
+      end
+      [self, @next]
     end
   end
 
-  # get the last included integer in the given range
-  # last_in(1..2) == 2
-  # last_in(1...3) == 2
-  def self.last_in range
-    if range.exclude_end?
-      range.end - 1
-    else
-      range.end
+  private def trisect_around! interval
+    tmp_next = @next
+    tmp_high = high
+
+    @interval.high = interval.low - 1
+    @next = IntervalList.new(
+      interval,
+      @values.clone,
+      IntervalList.new(
+        Interval.new(interval.high + 1, tmp_high),
+        @values.clone,
+        tmp_next
+      )
+    )
+  end
+
+  private def bisect_after! point
+    tmp_high = high
+    tmp_next = @next
+
+    @interval.high = point
+    @next = IntervalList.new(
+      Interval.new(point + 1, tmp_high),
+      @values.clone,
+      tmp_next
+    )
+  end
+
+  def to_s
+    "<#{@interval} #{@values} #{@next&.interval}>"
+  end
+end
+
+# Associates values with integer ranges.
+class IntervalTree
+
+  def initialize(entire_interval)
+    @interval_list = IntervalList.new(
+      entire_interval
+    )
+  end
+
+  def to_s
+    @interval_list.to_s
+  end
+
+  def insert(interval, value)
+    each_in_interval(interval) do |node|
+      node.split_around!(interval).each do |subnode|
+        subnode.values << value unless
+            interval.disjoint? subnode.interval
+      end
+    end
+  end
+
+  def each_in_interval(interval)
+    current = find_first(interval.low)
+    while current&.low&. <= interval.high
+      tmp_next = current.next
+      yield current
+      current = tmp_next
+    end
+  end
+
+  private def find_first(point)
+    #TODO: use a BST
+    current = @interval_list
+    while current&.next&.low&. <= point
+      current = current.next
+    end
+    current
+  end
+
+  def get interval
+    each_in_interval(interval) do |node|
+      yield node.interval, node.values
     end
   end
 end
--- ruby-solutions/spec/interval_tree.spec.rb
+++ ruby-solutions/spec/interval_tree.spec.rb
@@ -6,125 +6,117 @@ require 'interval_tree'
 describe IntervalTree do
 
   before do
-    @tree = IntervalTree.new(1..10)
+    @tree = IntervalTree.new Interval.new(1, 10)
   end
 
   describe '#insert' do
     it 'adds an interval with the given value' do
-      @tree.insert(1..10, "1..10")
+      @tree.insert(Interval.new(1, 10), "1..10")
 
-      assert_equal 1..10, @tree.range
-      assert_equal "1..10", @tree.values.first
-    end
-
-    it 'converts exclusive ranges to inclusive ranges' do
-      @tree.insert(1...11, "1...11")
-
-      assert_equal (1..10), @tree.range
-      assert_equal false, (1...11) == @tree.range
-      assert_equal "1...11", @tree.values.first
+      @tree.get(Interval.new(1, 10)) do |interval, values|
+        assert_equal 1, interval.low
+        assert_equal 10, interval.high
+        assert_equal 1, values.size
+        assert_equal true, values.include?("1..10")
+      end
     end
   end
 
-  describe '#query' do
+  describe '#get' do
     it 'gets all values intersecting the given range' do
-      @tree.insert(1..10, "1..10")
-      @tree.insert(2..9, "2..9")
-      @tree.insert(3..8, "3..8")
-      @tree.insert(2..2, "2..2")
-      @tree.insert(9..9, "9..9")
+      @tree.insert(Interval.new(1, 10), "1..10")
+      @tree.insert(Interval.new(2, 9), "2..9")
+      @tree.insert(Interval.new(3, 8), "3..8")
+      @tree.insert(Interval.new(2, 2), "2..2")
+      @tree.insert(Interval.new(9, 9), "9..9")
 
-      @tree.query(2..9).tap do |set|
-        assert_equal true, set.include?("1..10")
-        assert_equal true, set.include?("2..9")
-        assert_equal true, set.include?("3..8")
-        assert_equal true, set.include?("2..2")
-        assert_equal true, set.include?("9..9")
+      values = []
+      @tree.get(Interval.new(2, 9)) do |i, v|
+        values.push(*v)
       end
+
+      assert_equal true, values.include?("1..10")
+      assert_equal true, values.include?("2..9")
+      assert_equal true, values.include?("3..8")
+      assert_equal true, values.include?("2..2")
+      assert_equal true, values.include?("9..9")
     end
 
     it 'does not get values outside the range' do
-      @tree.insert(1..1, "1..1")
-      @tree.insert(10..10, "10..10")
+      @tree.insert(Interval.new(1, 1), "1..1")
+      @tree.insert(Interval.new(10, 10), "10..10")
 
-      @tree.query(2..9).tap do |set|
-        assert_equal true, set.empty?
+      v = []
+      @tree.get(Interval.new(2, 9)) do |i, v|
+        v.push(*v)
       end
-    end
 
-    it 'can query using exclusive ranges' do
-      @tree.insert(1..10, "1..10")
-      @tree.insert(3..8, "3..8")
-
-      @tree.query(1...3).tap do |set|
-        assert_equal true, set.include?("1..10")
-        assert_equal false, set.include?("3..8")
-      end
+      assert_equal true, v.empty?
     end
   end
 end
 
 # Some of the bench* methods are not available yet, so we're not using spec for
 # benchmarking
-class Benchmarks < Minitest::Benchmark
-  def self.bench_range
-    bench_exp 10_000, 1_000_000, 10
-  end
+# class Benchmarks < Minitest::Benchmark
+#   def self.bench_range
+#     bench_exp 10_000, 1_000_000, 10
+#   end
 
-  def bench_initialize
-    assert_performance_constant do |n|
-      100.times { IntervalTree.new 1..n }
-    end
-  end
+#   def bench_initialize
+#     assert_performance_constant do |n|
+#       100.times { IntervalTree.new n }
+#     end
+#   end
 
-  # Large ranges can be inserted without exploring as many nodes. If a range
-  # covers the root, it is inserted at the root.
-  def bench_insert_best_case
-    assert_performance_constant do |n|
-      @tree = IntervalTree.new 1..n
-      100.times { @tree.insert 1..n, :value }
-    end
-  end
+#   # Large ranges can be inserted without exploring as many nodes. If a range
+#   # covers the root, it is inserted at the root.
+#   def bench_insert_best_case
+#     assert_performance_constant do |n|
+#       @tree = IntervalTree.new n
+#       100.times { @tree.insert 1..n, :value }
+#     end
+#   end
 
-  # A range containing a single value has to be inserted in a leaf. This forces
-  # the tree to explore from root to leaf, creating approximately log(n) nodes
-  # on the way.
-  #
-  # 1_000 reps and relaxed threshold because the runtime growth is subtle.
-  def bench_insert_worse_case
-    assert_performance_logarithmic 0.9 do |n|
-      1000.times do
-        tree = IntervalTree.new 1..n
-        tree.insert 1..1, :value
-      end
-    end
-  end
+#   # A range containing a single value has to be inserted in a leaf. This forces
+#   # the tree to explore from root to leaf, creating approximately log(n) nodes
+#   # on the way.
+#   #
+#   # 1_000 reps and relaxed threshold because the runtime growth is subtle.
+#   def bench_insert_worse_case
+#     assert_performance_logarithmic 0.9 do |n|
+#       1000.times do
+#         tree = IntervalTree.new n
+#         tree.insert 1..1, :value
+#       end
+#     end
+#   end
 
-  # Lerger ranges are inserted closer to the root, so queries similarly do not
-  # need to descend as deep into the tree. We can find the root's range in
-  # constant time.
-  def bench_query_best_case
-    assert_performance_constant do |n|
-      tree = IntervalTree.new 1..n
-      tree.insert 1..n, :value
-      100.times do
-        tree.query 1..n
-      end
-    end
-  end
+#   # Lerger ranges are inserted closer to the root, so queries similarly do not
+#   # need to descend as deep into the tree. We can find the root's range in
+#   # constant time.
+#   def bench_get_best_case
+#     assert_performance_constant do |n|
+#       tree = IntervalTree.new n
+#       tree.insert 1..n, :value
+#       100.times do
+#         tree.get 1..n
+#       end
+#     end
+#   end
 
-  # Single-value intervals are inserted in leafs, requiring queries to descend
-  # deep into the tree to locate them. There are log(n) layers in a tree to
-  # descend through, so queries should be at worst logarithmic.
-  #
-  # 1_000 reps and relaxed threshold because the runtime growth is subtle.
-  def bench_query_worst_case
-    assert_performance_logarithmic 0.9 do |n|
-      tree = IntervalTree.new 1..n
-      tree.insert 1..1, :value
-      1000.times do
-        tree.query 1..1
-      end
-    end
-  end
-end
+#   # Single-value intervals are inserted in leafs, requiring queries to descend
+#   # deep into the tree to locate them. There are log(n) layers in a tree to
+#   # descend through, so queries should be at worst logarithmic.
+#   #
+#   # 1_000 reps and relaxed threshold because the runtime growth is subtle.
+#   def bench_get_worst_case
+#     assert_performance_logarithmic 0.9 do |n|
+#       tree = IntervalTree.new n
+#       tree.insert 1..1, :value
+#       1000.times do
+#         tree.get 1..1
+#       end
+#     end
+#   end
+# end
